f1db3650d195ecdfcf9d3a12891799d367b1d74f
vaibhav sawarbandhe <svv5498@gmail.com>
2026-06-21 23:48:29 +0530

    Initial backend commit

--- src/app.js
+++ src/app.js
@@ -12,7 +12,10 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: [
+    'http://localhost:3000',
+    'https://newportfolio-omega-navy.vercel.app'
+  ],
   credentials: true
 }));
 
